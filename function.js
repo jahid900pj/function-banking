0b4fbd846934967f01e663fc36ea5c13ad7fa473
jahidhasan234031 <jahidhasan234031@mail.com>
2022-02-13 17:29:26 +0600

    secend

--- function.js
+++ function.js
@@ -1,18 +1,15 @@
-// function getInputValue() {
-//     const DepositInput = document.getElementById('Deposit-input');
-//     const depositAmountText = DepositInput.value;
-//     const prevesDepositAmount = parseFloat(depositAmountText);
 
-//     return prevesDepositAmount;
-// }
-
-
-
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputAmountText = inputField.value;
+    const amountValue = parseFloat(inputAmountText);
+    inputField.value = '';
+    return amountValue;
+}
 
 document.getElementById('Deposit-button').addEventListener('click', function () {
-    const DepositInput = document.getElementById('Deposit-input');
-    const depositAmountText = DepositInput.value;
-    const prevesDepositAmount = parseFloat(depositAmountText);
+
+    const prevesDepositAmount = getInputValue('Deposit-input');
 
 
 
@@ -21,8 +18,6 @@ document.getElementById('Deposit-button').addEventListener('click', function () 
     const prevesisDepositTotal = parseFloat(depositTotalText);
     depositTotal.innerText = prevesisDepositTotal + prevesDepositAmount;
 
-
-    DepositInput.value = '';
 
     //update blance
 
@@ -38,22 +33,21 @@ document.getElementById('Deposit-button').addEventListener('click', function () 
 //Withdrow  
 
 document.getElementById('Withdraw-button').addEventListener('click', function () {
-    const withdrawInput = document.getElementById('Withdraw-input');
-    const withdrawAmountText = withdrawInput.value;
-    const previesWithdrawAmount = parseFloat(withdrawAmountText);
+
+    const withdrawAmount = getInputValue('Withdraw-input')
 
     const withdrawTotal = document.getElementById('withdraw-total');
     const withdrawTotalText = withdrawTotal.innerText;
     const previesWithdrawTotal = parseFloat(withdrawTotalText);
 
-    withdrawTotal.innerText = previesWithdrawAmount + previesWithdrawTotal;
-    withdrawInput.value = '';
+    withdrawTotal.innerText = withdrawAmount + previesWithdrawTotal;
+
 
     //update blance after withdraw
     const blanceTotal = document.getElementById('blance-total');
     const blanceTotalText = blanceTotal.innerText;
     const previesBlanceTotal = parseFloat(blanceTotalText);
-    blanceTotal.innerText = previesBlanceTotal - previesWithdrawAmount;
+    blanceTotal.innerText = previesBlanceTotal - withdrawAmount;
 
 
 
